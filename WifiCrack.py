2bb32b4b689d9c71edf366ced48e67d0cac9b852
Dhatchaneshwaran <68842635+Dhatchaneshwaran@users.noreply.github.com>
2021-04-30 19:09:12 +0530

    File_name : WifiCrack.py
    Funny wifi cracking. Make your friends fool by saying this as real wifi hacking tool.

---
+++ WifiCrack.py
@@ -0,0 +1,34 @@
+i=0
+print('Welcome to Wifi password cracker  !!\n')
+print("Disclaimer :")
+print(" **  With great power there is more responsiblity ")
+print("So don't misuse this tool, use it only for emergency purposes  **\n\n")
+print("MAKE SURE THAT WIFI IS ENABLED ON YOUR DEVICE")
+a =str(input("\n Enter the name of a Wifi network :"))
+for i in range(1,200000):
+      print("Identifying the MAC Address of the victom")
+      if i==200000:
+      	break
+for i in range(1,200000):      	
+      print("\nPreparing for BruteForce attack/~~~")
+      if i==200000:
+      	break
+for i in range(1,300000):      	
+      print("\nPassword is being crack [*|*|*|*|*|*|*|*|*|*]\n")
+      if i==300000:
+      	break
+for i in range(1,300000):       	
+    print("~~Password cracked sucessfully~~")
+    if i==100000:
+    	break
+for i in range(1,100000):
+      print("\nPlease wait...........until connect to a network\n")
+      if i==200000:
+          break
+for i in range(1,300000) :
+      print(" connecting network.........\n")
+      if i==300000:
+         break
+
+print("[ ### IMPORTANT INSTRUCTION ###  :]")
+print("#~~ nee elaam poranthey oru sabakedu, onaku oc wifi oru keda ~~#")
